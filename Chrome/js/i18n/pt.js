f58e918afa6a3df418d4d3deb260a35f06e4733b
Joe Cloudwalker <joe.isaacs.pm@gmail.com>
2016-04-26 16:34:00 +0100

    Turkish percentage fixed

--- Chrome/js/i18n/pt.js
+++ Chrome/js/i18n/pt.js
@@ -2,11 +2,11 @@
 
 function lang_en() {
 	this.AF_Global					= "Global";
-	this.FI_Filter					= "Filter";
-	this.FI_FilterItems				= "Filter Items";
-	this.FI_PlaceHolder				= "Character ID";
-	this.FR_ShowGroup				= "Show Group";
-	this.FR_HideGroup				= "Hide Group";
+	this.FI_Filter					= "Filtrar";
+	this.FI_FilterItems				= "Filtrar Itens";
+	this.FI_PlaceHolder				= "ID da personagem";
+	this.FR_ShowGroup				= "Mostrar groupo";
+	this.FR_HideGroup				= "Esconder groupo";
 	this.SP_121						= "Gossip on the phone";
 	this.SP_24						= "Wazzup call";
 	this.SP_25						= "Lover call";
@@ -15,8 +15,8 @@ function lang_en() {
 	this.SP_61						= "SMS friendly text";
 	this.SP_73						= "Flirty Phone call";
 	this.SP_74						= "Flirty SMS";
-	this.SP_9999					= "Dont Call";
-	this.SP_CallButton				= "Call everyone";
+	this.SP_9999					= "Não ligar";
+	this.SP_CallButton				= "Ligar para todo mundo";
 	this.SP_ReportBugs				= "     Joe Cloudwalker's SpeedCalling for Popmundo";
 	this.ABOUT_About				= 'This extention was created as an addon to the <a href="http://popmundo.com" target="_BLANK">Popmundo</a> roleplaying game by <a href="http://www.popmundo.com/World/Popmundo.aspx/Character/3248185" target="_BLANK">Joe Cloudwalker</a> and is not affiliated with, sponsored by, or endorsed by <em>Spelkultur i Sverige AB</em> in any way. Any problems, please contact <a href="http://www.popmundo.com/World/Popmundo.aspx/Character/3248185" target="_BLANK">the developer</a> in game.</p><p>Use the <a href="#" id="aboutOptions" target="_BLANK">Options Page</a> to configure all features.';
 	this.ABOUT_Copyright			= "Copyright & Specs";
@@ -24,5 +24,5 @@ function lang_en() {
 	this.ABOUT_OriginalCode			= 'The original code of this extension can be found at <a href="https://github.com/joe-isaacs/speedcalling-popmundo" target="_BLANK">github.com/joe-isaacs/speedcalling-popmundo</a>.';
 	this.ABOUT_Icones01				= '<a href="https://www.iconfinder.com/iconsets/20-flat-icons" target="_BLANK">20 Flat icons set</a> developed by <a href="http://www.graphicsfuel.com/" target="_BLANK">Graphics Fuel</a>';
 	this.ABOUT_Icones02				= '<a href="https://www.iconfinder.com/iconsets/softwaredemo" target="_BLANK">Vista Style Base Software</a> developed by <a href="http://www.icons-land.com/" target="_BLANK">Icons Land</a>';
-	this.ABOUT_Licence				= '<span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">SpeedCalling</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/joe-isaacs/speedcalling-popmundo" property="cc:attributionName" rel="cc:attributionURL">Joe Cloudwalker</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.<br /><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a>';
+	this.ABOUT_Licence				= '<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Licença Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />Este trabalho de <a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/joe-isaacs/speedcalling-popmundo" property="cc:attributionName" rel="cc:attributionURL">Joe Cloudwalker</a>, está licenciado com uma Licença <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons - Atribuição-CompartilhaIgual 4.0 Internacional</a>.<br />Baseado no trabalho disponível em <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/joe-isaacs/speedcalling-popmundo" rel="dct:source">https://github.com/joe-isaacs/speedcalling-popmundo</a>.';
 }
--- Chrome/js/web/valueToProgressbar.js
+++ Chrome/js/web/valueToProgressbar.js
@@ -1,36 +1,43 @@
 // Adds numbers to Progressbars in all pages
 if( globalCanRender( [ "\/World\/Popmundo.aspx\/.*" ], [ 'QualityToValue_showPercentages' ] ) ) {
-	execValueToProgressbar();
+	execValueToProgressbar( );
 }
 
-function execValueToProgressbar() {
-	$( 'div[class*="rogressBar"]' ).each( function() {
+function execValueToProgressbar( ) {
+	$( 'div[class*="rogressBar"]' ).each( function( ) {
 
-		//Creates the hide button
+//Creates the hide button
 		var mySpan = document.createElement( "span" );
 		mySpan.className = "progressBar";
-
 		var tmpValue = $( this ).attr( 'title' );
-		tmpValue = tmpValue.substr( 0, tmpValue.indexOf( "%" ) );
-		mySpan.textContent = tmpValue + '%';
-		$( this ).children( "div:first" ).append( mySpan );
-
-	} );
-
-	$( '.plusMinusBar' ).each( function() {
-
-		//Creates the hide button
-		var mySpan = document.createElement( "span" );
-
-		var tmpValue = $( this ).attr( 'title' );
-		tmpValue = tmpValue.substr( 0, tmpValue.indexOf( "%" ) );
-		mySpan.textContent = tmpValue + '%';
-
-		if( tmpValue >= 0 ) {
-			$( this ).children( "div" ).eq( 1 ).children().append( mySpan );
+		//Percentage after number
+		if( tmpValue.substr( 0, tmpValue.indexOf( "%" ) > 0 ) ) {
+			tmpValue = tmpValue.substr( 0, tmpValue.indexOf( "%" ) );			
 		} else {
-			$( this ).children( "div" ).eq( 0 ).children().append( mySpan );
+			tmpValue = tmpValue.substr( 1 );
+		}	
+		mySpan.textContent = tmpValue + '%';
+
+
+		$( this ).children( "div:first" ).append( mySpan );
+	} );
+	$( '.plusMinusBar' ).each( function( ) {
+
+//Creates the hide button
+		var mySpan = document.createElement( "span" );
+		var tmpValue = $( this ).attr( 'title' );
+		var tmpValue = $( this ).attr( 'title' );
+		//Percentage after number
+		if( tmpValue.substr( 0, tmpValue.indexOf( "%" ) > 0 ) ) {
+			tmpValue = tmpValue.substr( 0, tmpValue.indexOf( "%" ) );			
+		} else {
+			tmpValue = tmpValue.substr( 1 );
+		}	
+		mySpan.textContent = tmpValue + '%';
+		if( tmpValue >= 0 ) {
+			$( this ).children( "div" ).eq( 1 ).children( ).append( mySpan );
+		} else {
+			$( this ).children( "div" ).eq( 0 ).children( ).append( mySpan );
 		}
 	} );
-
 }
